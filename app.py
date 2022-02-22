5a0aa167f8a7b64e21c5acaf804e53a278c2e2e6
VedhaSankar <vedhasankar26@gmail.com>
2022-02-22 19:53:03 +0530

    started with upload page

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 from dotenv import load_dotenv
 import os
+from werkzeug.utils import secure_filename
 
 load_dotenv()
 
@@ -9,6 +10,7 @@ app = Flask(__name__)
 ADMIN_USERNAME  = os.environ.get('ADMIN_USERNAME')
 ADMIN_PASSWORD  = os.environ.get('ADMIN_PASSWORD')
 PORT            = os.environ.get('PORT')
+app.config["UPLOAD_FOLDER"] = "static/"
 
 @app.route('/', methods=['GET'])
 def start():
@@ -22,12 +24,28 @@ def home():
     psw = request.values.get('psw')
     login_type = request.form.getlist('checkbox')
 
+
     if uname == ADMIN_USERNAME and psw == ADMIN_PASSWORD:
         return render_template('home.html')
 
     else:
         return render_template('index.html', error='Invalid username or password')
 
+
+
+@app.route('/upload', methods = ['GET', 'POST'])
+def save_file():
+    if request.method == 'POST':
+        f = request.files['file']
+        filename = secure_filename(f.filename)
+
+        f.save(app.config['UPLOAD_FOLDER'] + filename)
+
+        file = open(app.config['UPLOAD_FOLDER'] + filename,"r")
+        content = file.read()
+        
+        
+    return render_template('upload.html', content=content) 
 
 @app.route('/ping')
 def ping():
