25acb5dfbd996bf2f65ed5a66b0f552f7fc8a21d
VedhaSankar <vedhasankar26@gmail.com>
2022-03-04 12:34:29 +0530

    need to flash a message incorrect extension if not zip and shouldnt upload all details to mongo

--- app.py
+++ app.py
@@ -12,11 +12,11 @@ load_dotenv()
 
 app = Flask(__name__)
 
-ADMIN_USERNAME  = os.environ.get('ADMIN_USERNAME')
-ADMIN_PASSWORD  = os.environ.get('ADMIN_PASSWORD')
-MONGO_URI       = os.environ.get('MONGO_URI')
-PORT            = os.environ.get('PORT')
-
+ADMIN_USERNAME      = os.environ.get('ADMIN_USERNAME')
+ADMIN_PASSWORD      = os.environ.get('ADMIN_PASSWORD')
+MONGO_URI           = os.environ.get('MONGO_URI')
+PORT                = os.environ.get('PORT')
+ALLOWED_EXTENSIONS  = {'zip'}
 
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
@@ -25,6 +25,12 @@ app.config["UPLOAD_FOLDER"] = "uploads/"
 client = MongoClient(MONGO_URI)  
 DB_NAME = 'prohost'
 database = client[DB_NAME]
+
+
+def allowed_file(filename):
+    
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route('/', methods=['GET', 'POST'])
 def start():
@@ -76,17 +82,45 @@ def get_project_details():
         x = new_collection.insert_one(project_dict)
         print(x)
 
-        if 'files[]' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
+        # if 'files[]' not in request.files:
+        #     flash('No file part')
+        #     return redirect(request.url)
 
-        files = request.files.getlist('files[]')
+        # files = request.files.getlist('files[]')
 
-        for file in files:
-            if file:
+        # for file in files:
+        #     if file:
+        #         filename = secure_filename(file.filename)
+        #         file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+        if request.method == 'POST':
+        # check if the post request has the file part
+            if 'file' not in request.files:
+
+                flash('No file part')
+                return redirect(request.url)
+
+            file = request.files['file']
+
+            # If the user does not select a file, the browser submits an
+            # empty file without a filename.
+
+            if file.filename == '':
+
+                flash('No selected file')
+                return redirect(request.url)
+
+            if file and allowed_file(file.filename):
+
                 filename = secure_filename(file.filename)
                 file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            gcp_upload.upload_blob(f'uploads/{filename}', f'project_{current_project_id}/{filename}')
+                gcp_upload.upload_blob(f'uploads/{filename}', f'project_{current_project_id}/{filename}')
+
+                return render_template('form.html')
+                # return redirect(url_for('download_file', name=filename))
+            else:
+                flash('Allowed file type is .zip')
+                return redirect(request.url)
 
     return render_template('form.html')
 
